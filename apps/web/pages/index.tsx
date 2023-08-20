cb5f0aa774127c8b2ebd4208edf9197d9fc35d77
Jacek Smolak <jacek.smolak@gmail.com>
2023-08-20 19:56:11 +0200

    Setup connection to DB

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,13 @@
+import { prisma } from "@urlshare/db/prisma/client";
 import { Button } from "@urlshare/ui/components/ui/button";
 
-export default function Page() {
+export default function Page(props: any) {
+  console.log(props);
+
   return (
     <section className="flex w-40 flex-col gap-1">
       <h1>Web</h1>
+      {/*<p>URLs count: ${urlsCount}</p>*/}
       <Button>Default</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="secondary">Secondary</Button>
@@ -12,3 +16,13 @@ export default function Page() {
     </section>
   );
 }
+
+export const getServerSideProps = async () => {
+  const urlCount = await prisma.url.count();
+
+  return {
+    props: {
+      urlCount,
+    },
+  };
+};
