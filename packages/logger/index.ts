bf140bff83a2c5d66a3d6639b177f8eed088aa73
Jacek Smolak <jacek.smolak@gmail.com>
2023-08-21 23:37:36 +0200

    Add logger

---
+++ packages/logger/index.ts
@@ -0,0 +1,7 @@
+import pino from "pino";
+
+// TODO: remove the `|| "info"` once I will know how to pass env to vitest
+// TODO: idea: move to env-based configuration?
+const level = process.env.LOG_LEVEL || "info";
+
+export const logger = pino({ nestedKey: "payload", browser: { asObject: true }, level });
