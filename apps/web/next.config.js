3d7bb18360afa5adb9b446bbad6296a184dd9f7f
Jacek Smolak <jacek.smolak@gmail.com>
2023-08-27 17:14:53 +0200

    Add missing configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@urlshare/ui"],
+  serverRuntimeConfig: {
+    feedQueueApiKey: process.env.FEED_QUEUE_API_KEY,
+    urlQueueApiKey: process.env.URL_QUEUE_API_KEY,
+    userFeedList: {
+      itemsPerPage: 30,
+    },
+    rss: {
+      itemsPerUserChannel: 20,
+    },
+  },
 };
