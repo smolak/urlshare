eed061215e09f218935c33229752492633220bd1
Jacek Smolak <jacek.smolak@gmail.com>
2023-08-28 23:05:36 +0200

    Make sure not allowed username are ... not allowed.

---
+++ packages/web-app/user-profile-data/router/procedures/create-user-profile-data.schema.test.ts
@@ -0,0 +1,44 @@
+import { expect } from "vitest";
+import { SafeParseError } from "zod";
+
+import { generateApiKey } from "../../../user/utils/generate-api-key";
+import { CreateUserProfileDataSchema, createUserProfileDataSchema } from "./create-user-profile-data.schema";
+
+function generateCaseCombinations(word: string) {
+  function backtrack(index: number, currentCombination: string) {
+    if (index === word.length) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const letter = word[index];
+
+    backtrack(index + 1, currentCombination + letter.toLowerCase());
+    backtrack(index + 1, currentCombination + letter.toUpperCase());
+  }
+
+  const combinations: string[] = [];
+  backtrack(0, "");
+
+  return combinations;
+}
+
+const apiKey = generateApiKey();
+
+describe("createUserProfileDataSchema", () => {
+  it("fails validation when using not allowed usernames", () => {
+    const notAllowedUsernames = [...generateCaseCombinations("admin"), ...generateCaseCombinations("urlshare")];
+
+    notAllowedUsernames.forEach((username) => {
+      const data = {
+        username,
+        apiKey,
+      };
+
+      const result = createUserProfileDataSchema.safeParse(data) as SafeParseError<CreateUserProfileDataSchema>;
+
+      expect(result.success).toEqual(false);
+      expect(result.error.format().username?._errors).toContain("Username not allowed.");
+    });
+  });
+});
--- packages/web-app/user-profile-data/router/procedures/create-user-profile-data.schema.ts
+++ packages/web-app/user-profile-data/router/procedures/create-user-profile-data.schema.ts
@@ -4,7 +4,18 @@ import { apiKeySchema, usernameSchema } from "../../schemas/user-profile-data.sc
 
 export type CreateUserProfileDataSchema = z.infer<typeof createUserProfileDataSchema>;
 
+const NOT_ALLOWED_NORMALIZED_USERNAMES = ["admin", "urlshare"];
+
+const restrictedUsernameSchema = usernameSchema.refine(
+  (username) => {
+    return !NOT_ALLOWED_NORMALIZED_USERNAMES.includes(username.toLowerCase());
+  },
+  {
+    message: "Username not allowed.",
+  }
+);
+
 export const createUserProfileDataSchema = z.object({
   apiKey: apiKeySchema,
-  username: usernameSchema,
+  username: restrictedUsernameSchema,
 });
