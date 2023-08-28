1c697181cd7234e6880489ef51343bf23b84fc4a
Jacek Smolak <jacek.smolak@gmail.com>
2023-08-28 22:16:53 +0200

    404 should have its own layout as well

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -1,7 +1,8 @@
+import { ThreeColumnLayout } from "@urlshare/web-app/ui/three-column.layout";
 import { NextPage } from "next";
 
 const Custom404: NextPage = () => {
-  return <h2>404...</h2>;
+  return <ThreeColumnLayout mainContent={<h2>404...</h2>} />;
 };
 
 export default Custom404;
