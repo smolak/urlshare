874cfbab22ce8c99f7798f3f4fb64749e402b448
Jacek Smolak <jacek.smolak@gmail.com>
2023-09-23 23:39:05 +0200

    Darn it ... need to fix this asap

--- apps/web/pages/api/trpc/[trpc].ts
+++ apps/web/pages/api/trpc/[trpc].ts
@@ -2,7 +2,10 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "@urlshare/web-app/trpc/app-router";
 import { createTRPCContext } from "@urlshare/web-app/trpc/server";
 
+// TODO there is some TS incompatibility with the router created and expected. It wasn't the case till recently :/
+
 export default createNextApiHandler({
+  // @ts-ignore
   router: appRouter,
   createContext: createTRPCContext,
   onError:
