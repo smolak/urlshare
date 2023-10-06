b8b10e047b921a4039d450e12c5c2908dd933e58
Jacek Smolak <jacek.smolak@gmail.com>
2023-10-06 22:49:49 +0200

    tRPC TS errors got fixed with this update

--- apps/web/pages/api/trpc/[trpc].ts
+++ apps/web/pages/api/trpc/[trpc].ts
@@ -2,11 +2,7 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "@urlshare/web-app/trpc/app-router";
 import { createTRPCContext } from "@urlshare/web-app/trpc/server";
 
-// TODO there is some TS incompatibility with the router created and expected. It wasn't the case till recently :/
-// I've found out that it has something to do with Turborepo ... but I can't find what.
-// Will get back to this from time to time to fix this.
 export default createNextApiHandler({
-  // @ts-ignore
   router: appRouter,
   createContext: createTRPCContext,
   onError:
