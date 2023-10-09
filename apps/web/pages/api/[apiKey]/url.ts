b6e2a6af83bf9871dc3d78e95c5e0594b1033971
Jacek Smolak <jacek.smolak@gmail.com>
2023-10-09 14:20:20 +0200

    Allow CORS

--- apps/web/pages/api/[apiKey]/url.ts
+++ apps/web/pages/api/[apiKey]/url.ts
@@ -7,6 +7,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   switch (method) {
     case "POST":
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+      );
+
       await addUrlWithApiKeyHandler(req, res);
       break;
 
