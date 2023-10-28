4c919accdbf2e5debc081b0d29f50a1d381d2deb
Jacek Smolak <jacek.smolak@gmail.com>
2023-10-28 23:44:01 +0200

    Register routers

--- packages/web-app/trpc/app-router.ts
+++ packages/web-app/trpc/app-router.ts
@@ -1,14 +1,18 @@
+import { categoryRouter } from "../category/router/category";
 import { feedRouter } from "../feed/router/feed";
 import { followUserRouter } from "../follow-user/router/follow-user";
 import { urlRouter } from "../url/router/url";
 import { userProfileDataRouter } from "../user-profile-data/router/user-profile-data";
+import { userUrlCategoryRouter } from "../user-url-category/router/user-url-category";
 import { createTRPCRouter } from "./server";
 
 export const appRouter = createTRPCRouter({
+  category: categoryRouter,
   feed: feedRouter,
   followUser: followUserRouter,
   url: urlRouter,
   userProfileData: userProfileDataRouter,
+  userUrlCategory: userUrlCategoryRouter,
 });
 
 export type AppRouter = typeof appRouter;
