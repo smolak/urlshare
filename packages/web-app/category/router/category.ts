2106e95d1fb12f223cf7689b38f8c740e96aea09
Jacek Smolak <jacek.smolak@gmail.com>
2023-10-29 18:13:40 +0100

    Add getMyCategories procedure

--- packages/web-app/category/router/category.ts
+++ packages/web-app/category/router/category.ts
@@ -1,12 +1,14 @@
 import { createTRPCRouter } from "../../trpc/server";
 import { createCategory } from "./procedures/create-category";
 import { deleteCategory } from "./procedures/delete-category";
+import { getMyCategories } from "./procedures/get-my-categories";
 import { getUserCategories } from "./procedures/get-user-categories";
 import { updateCategory } from "./procedures/update-category";
 
 export const categoryRouter = createTRPCRouter({
   createCategory,
+  deleteCategory,
+  getMyCategories,
   getUserCategories,
   updateCategory,
-  deleteCategory,
 });
