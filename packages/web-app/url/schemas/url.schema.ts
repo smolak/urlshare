0527f4dd708734381846111e36e7c7fd3447bdf7
Jacek Smolak <jacek.smolak@gmail.com>
2023-11-01 21:28:07 +0100

    Improve adding URL on homepage

--- packages/web-app/url/schemas/url.schema.ts
+++ packages/web-app/url/schemas/url.schema.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
+const MAX_URL_LENGTH = 500;
+
 export const urlSchema = z
   .string()
   .trim()
-  .url()
+  .min(1, { message: "URL can't be empty." })
+  .url({ message: "Value is not a URL." })
+  .startsWith("https://", { message: "Only https:// URLs allowed." })
   .refine(
-    (val) => val.startsWith("https://"),
-    (val) => ({ message: `Only https:// URLs allowed at the moment. Passed URL: ${val}` })
-  )
-  .refine(
-    (val) => val.length <= 500,
-    (val) => ({ message: `Oh NOES! That URL is too long. Detected ${val.length} characters and max 500 is allowed.` })
+    (val) => val.length <= MAX_URL_LENGTH,
+    (val) => ({ message: `URL is too long. Detected ${val.length} characters and max ${MAX_URL_LENGTH} is allowed.` })
   );
