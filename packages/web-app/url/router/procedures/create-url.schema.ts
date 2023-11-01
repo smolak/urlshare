98ae320cb8b0fcbc1439f3ff9d6b2a6bb0a1ba3e
Jacek Smolak <jacek.smolak@gmail.com>
2023-11-01 21:28:07 +0100

    Improve adding URL on homepage

--- packages/web-app/url/router/procedures/create-url.schema.ts
+++ packages/web-app/url/router/procedures/create-url.schema.ts
@@ -2,6 +2,8 @@ import z from "zod";
 
 import { urlSchema } from "../../schemas/url.schema";
 
+export type CreateUrlSchema = z.infer<typeof createUrlSchema>;
+
 export const createUrlSchema = z.object({
   url: urlSchema,
 });
--- packages/web-app/url/ui/add-url/index.tsx
+++ packages/web-app/url/ui/add-url/index.tsx
@@ -1,15 +1,27 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@urlshare/ui/design-system/ui/button";
 import { Input } from "@urlshare/ui/design-system/ui/input";
 import { cn } from "@urlshare/ui/utils";
-import { Plus } from "lucide-react";
-import { useEffect, useState } from "react";
+import { CheckCircle, Plus } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { FiCheckCircle } from "react-icons/fi";
 
 import { api } from "../../../trpc/client";
+import { CreateUrlSchema, createUrlSchema } from "../../router/procedures/create-url.schema";
 
 export const AddUrl = () => {
-  const { register, handleSubmit, resetField, setFocus } = useForm();
+  const {
+    formState: { errors },
+    getValues,
+    handleSubmit,
+    register,
+    resetField,
+    setFocus,
+  } = useForm<CreateUrlSchema>({
+    resolver: zodResolver(createUrlSchema),
+    mode: "onSubmit",
+  });
+  const [errorResponse, setErrorResponse] = useState("");
   const [addedUrl, setAddedUrl] = useState("");
   const { mutate: addUrl, isLoading } = api.url.createUrl.useMutation({
     onSuccess: (data) => {
@@ -18,12 +30,30 @@ export const AddUrl = () => {
       resetField("url");
       setFocus("url");
     },
-    onError: () => {},
+    onError: (error) => {
+      setErrorResponse(error.message);
+    },
   });
 
   useEffect(() => {
     setFocus("url");
   }, [setFocus]);
+
+  useEffect(() => {
+    if (errorResponse !== "") {
+      setFocus("url");
+    }
+  }, [setFocus, errorResponse]);
+
+  useEffect(() => {
+    if (addedUrl !== "") {
+      const timeout = setTimeout(() => {
+        setAddedUrl("");
+      }, 5_000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [addedUrl, setAddedUrl]);
 
   const onSubmit = (values: FieldValues) => {
     setAddedUrl("");
@@ -34,8 +64,8 @@ export const AddUrl = () => {
 
   return (
     <div className="w-full">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex w-full items-center justify-center space-x-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="relative">
+        <div className="flex items-center gap-2">
           <Input
             {...register("url")}
             type="url"
@@ -43,21 +73,38 @@ export const AddUrl = () => {
             disabled={isLoading}
             placeholder="https://..."
             className="max-w-md"
+            onBlur={() => {
+              setErrorResponse("");
+
+              const { url } = getValues();
+
+              if (url === "") {
+                resetField("url");
+              }
+            }}
           />
           <Button type="submit" disabled={isLoading} className={cn("h-9 gap-1", { loading: isLoading })}>
             <Plus size={18} />
             <span>Add</span>
           </Button>
         </div>
+        {errors?.url?.message || errorResponse !== "" ? (
+          <p className="absolute mt-1 rounded rounded-md bg-red-50 px-2 py-1 text-sm text-red-600">
+            {errors?.url?.message || errorResponse}
+          </p>
+        ) : null}
+        {addedUrl !== "" && (
+          <p
+            className="absolute mt-1 rounded rounded-md bg-green-50 px-2 py-1 text-sm text-green-600"
+            onClick={() => setAddedUrl("")}
+          >
+            <span className="flex items-center gap-2">
+              <CheckCircle size={13} />
+              <span>URL added. It will be live soon.</span>
+            </span>
+          </p>
+        )}
       </form>
-      {addedUrl !== "" && (
-        <div className="" onClick={() => setAddedUrl("")}>
-          <div>
-            <FiCheckCircle />
-            <span>URL added. It will be live soon.</span>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
