7668971070576c57e77cee6bb5191a166d68d505
Jacek Smolak <jacek.smolak@gmail.com>
2023-11-09 22:16:00 +0100

    Fix component usage

--- apps/web/pages/settings/index.tsx
+++ apps/web/pages/settings/index.tsx
@@ -2,15 +2,9 @@ import { SettingsLayout } from "@urlshare/web-app/settings/ui/settings.layout";
 import { useSession } from "next-auth/react";
 
 const SettingsProfile = () => {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
 
-  return (
-    status === "authenticated" && (
-      <SettingsLayout title="Settings" user={session.user}>
-        To be added...
-      </SettingsLayout>
-    )
-  );
+  return status === "authenticated" && <SettingsLayout title="Settings">To be added...</SettingsLayout>;
 };
 
 export default SettingsProfile;
