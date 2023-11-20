cacde194ae65c1d131288c15900804db3c415ea5
Jacek Smolak <jacek.smolak@gmail.com>
2023-11-20 21:51:42 +0100

    I need to figure out a better way for this

--- packages/web-app/constants.ts
+++ packages/web-app/constants.ts
@@ -1,5 +1,4 @@
 export const WEB_APP_DOMAIN = "urlshare.app";
-// export const WEB_APP_BASE_URL = `https://${WEB_APP_DOMAIN}`;
-export const WEB_APP_BASE_URL = `http://localhost:3000`;
+export const WEB_APP_BASE_URL = `https://${WEB_APP_DOMAIN}`;
 export const WEB_APP_API_BASE_URL = `${WEB_APP_BASE_URL}/api`;
 export const REPOSITORY_URL = "https://github.com/smolak/urlshare";
